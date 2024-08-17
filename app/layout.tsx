b28b9ddffbef62b25d9a4111f3ec214cd820347b
Okereke Clement Kalu <inspikalu@gmail.com>
2024-08-17 09:51:42 +0100

    Added preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
         <meta name="dscvr:canvas:version" content="vNext" />
         <meta
           name="og:image"
-          content="https://my-canvas.com/preview-image.png"
+          content="https://meme-generator-orpin-iota.vercel.app/meme.png"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
